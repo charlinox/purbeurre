dffcc3bbce7546c0cc7a7da995e1725b01db5b7a
Thierry Chappuis <thierry@chappuis.io>
2020-02-04 15:59:26 +0100

    Fix of the configuration error

--- products/views.py
+++ products/views.py
@@ -1,10 +1,14 @@
 """Contains function used for views app search."""
 from django.shortcuts import render
 
-from .forms import search
+from .forms import SearchForm
 
 
 def index(request):
     """Display the site index."""
-    return render(request, 'search/index.html', {'form': search()})
+    return render(request, "search/index.html", {"form": SearchForm()})
 
+
+def result(request):
+    """Display the site index."""
+    return render(request, "search/index.html", {"form": SearchForm()})
