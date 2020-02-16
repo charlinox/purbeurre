7ebd726537dad68fd46c01b183a877ec49de2dc2
charlinox <charles.casassus@gmail.com>
2020-02-16 16:27:36 +0100

    error categories_id

--- products/substitute.py
+++ products/substitute.py
@@ -1,4 +1,4 @@
-from django.db.models import Q, Subquery
+from django.db.models import Q, Subquery, Count
 
 from .models import Product
 
@@ -6,13 +6,12 @@ from .models import Product
 def substitute(food_search):
     """ Propose a substitute for the search food """
 
-    categories = food_search.categories.all()
+    search_categories = food_search.categories.all()
     food_substitute = Product.objects.filter(
     ~Q(id=food_search.id),
-    Q(categories_id__in=Subquery(categories.values("id"))),
+    Q(categories_id__in=Subquery(search_categories.values("id"))),
     Q(nutrition_grade__lt=food_search.nutrition_grade),
     ).values("id", "name", "nutrition_grade", "url").annotate(count=Count("id"))
-
 
 
     if food_substitute:
