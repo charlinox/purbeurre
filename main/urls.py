8c4d5f927d032c40da63753991e9eed7c0620238
charlinox <charles.casassus@gmail.com>
2020-02-24 19:11:15 +0100

    problem with favorites

--- main/urls.py
+++ main/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path('mention_legale', views.legalmention, name='legal_mention'),
+    path('mention_legale', views.legalmention, name='legal_mention'),
 ]
